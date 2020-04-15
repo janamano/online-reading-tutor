d81d3baa7d39995f905cf1dc3035263ac033af5e
Natasha DeCoste <natasha.decoste@gmail.com>
2020-04-15 18:55:51 -0400

    merge with master

--- App.js
+++ App.js
@@ -11,18 +11,29 @@ import LessonMapScreen from "./src/screens/LessonMapScreen";
 import GameScreen from "./src/screens/GameScreen";
 import LessonScreen from "./src/screens/LessonScreen";
 
+//import styles
+import variables from "./src/styles/variables";
+
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" options={{ title: "Home" }} component={HomeScreen} />
-        <Stack.Screen name="Lessons" options={{ title: "Lessons" }} component={LessonMapScreen} />
-        <Stack.Screen name="Game" options={{ title: "Game" }} component={GameScreen} />
-        <Stack.Screen name="Lesson" options={{ title: "Lesson Simulation" }} component={LessonScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{
+          headerStyle: {
+            backgroundColor: variables.palette.gray.uiBackground,
+          },
+          headerTitleStyle: {
+            fontWeight: 'bold',
+            color: variables.palette.white.primary,
+          },
+        }}>
+          <Stack.Screen name="Home" options={{ title: "Home" }} component={HomeScreen} />
+          <Stack.Screen name="Lessons" options={{ title: "Lessons" }} component={LessonMapScreen} />
+          <Stack.Screen name="Game" options={{ title: "Game" }} component={GameScreen} />
+          <Stack.Screen name="Lesson" options={{ title: "Lesson Simulation" }} component={LessonScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
