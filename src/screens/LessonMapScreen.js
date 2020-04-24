3daf51c7722bde7474cb75f1d07768674db455b6
Ramya Prasad <31141615+rampras3@users.noreply.github.com>
2020-04-24 11:57:32 -0400

    basic carousel implementation (#36)
    * basic carousel
    
    * Update card
    
    * use local button component and move it out of cards
    
    * Update src/components/Carousel.js
    
    Co-Authored-By: Hussein Fahmy <husseinfahmy@users.noreply.github.com>
    
    * Update src/components/Carousel.js
    
    Co-Authored-By: Hussein Fahmy <husseinfahmy@users.noreply.github.com>
    
    Co-authored-by: Ramya Prasad <Ramya.R.Prasad@ibm.com>
    Co-authored-by: Hussein Fahmy <husseinfahmy@users.noreply.github.com>

--- src/screens/LessonMapScreen.js
+++ src/screens/LessonMapScreen.js
@@ -1,15 +1,38 @@
 import * as React from 'react';
-import { Text, View, Button } from 'react-native';
+import { StyleSheet,Text, View, Button } from 'react-native';
+import CarouselCards from '../components/Carousel.js';
 
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'black',
+    },
+    button: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    },
+    backgroundImage:{
+        flex: 1,
+        width: '100%',
+        height: '100%',
+        justifyContent: "center",
+        alignItems: "center",
+        opacity: 0.7
+    },
+});
 function LessonMapScreen({ navigation }) {
     return (
-        <View>
-            <Text>Lessons/Worlds screen</Text>
-            <Button title="Click here to start a 'world'"
-                onPress={() => navigation.navigate('Game')}
-            ></Button>
+        <View style= { styles.container }>
+                <View>
+                    <CarouselCards navigation = {navigation}/>
+
+                </View>
         </View>
+
     );
 }
 
