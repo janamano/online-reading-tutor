953ae4e38560211f6968bbb9c4d1fadae79fb25d
Natasha DeCoste <natasha.decoste@gmail.com>
2020-04-26 14:50:08 -0400

    lint globals vars

--- src/styles/global.js
+++ src/styles/global.js
@@ -1,28 +1,28 @@
+import { StyleSheet } from "react-native";
 import variables from "./variables";
 
 // global styles
-import { StyleSheet } from 'react-native';
 
 // global shared component/screen styles
 export default StyleSheet.create({
-    primaryBtn: {
-        marginRight: 40,
-        marginLeft: 40,
-        marginTop: 20,
-        marginBottom: 20,
-        paddingTop: 20,
-        paddingBottom: 20,
-        backgroundColor: variables.palette.blue.primary,
-        borderRadius: 50
-    },
-    primaryBtnText: {
-        paddingLeft: 15,
-        paddingRight: 15,
-        color: variables.palette.white.primary,
-        textAlign: "center",
-        fontSize: 18
-    },
-    appContainer: {
-        backgroundColor: variables.palette.gray.uiBackground,
-    }
+  primaryBtn: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginTop: 20,
+    marginBottom: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+    backgroundColor: variables.palette.blue.primary,
+    borderRadius: 50
+  },
+  primaryBtnText: {
+    paddingLeft: 15,
+    paddingRight: 15,
+    color: variables.palette.white.primary,
+    textAlign: "center",
+    fontSize: 18
+  },
+  appContainer: {
+    backgroundColor: variables.palette.gray.uiBackground
+  }
 });
--- src/styles/variables.js
+++ src/styles/variables.js
@@ -4,23 +4,23 @@ export default variables = {
     purple: {
       lightest: "#9766C1", // lightest
       light: "#7840A8",
-      primary: "#65269C",	// Main Primary color */
+      primary: "#65269C", // Main Primary color */
       darker: "#4E1381",
-      darkest: "#3A0766",
+      darkest: "#3A0766"
     },
     blue: {
       lightest: "#776FC5", // lightest
       light: "#534AAD",
       primary: "#3A30A1",
       darker: "#251C85",
-      darkest: "#160E6A",
+      darkest: "#160E6A"
     },
     pink: {
       lightest: "#D268AF", // lightest
       light: "#BE4095",
       primary: "#B12182",
       darker: "#930C67",
-      darkest: "#74004E",
+      darkest: "#74004E"
     },
     gray: {
       uiBackground: "#211E24" // mid/dark
@@ -29,5 +29,4 @@ export default variables = {
       primary: "#FFF"
     }
   }
-}
-
+};
