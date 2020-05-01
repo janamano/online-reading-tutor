08f17313fab8d7f317cf6a7ff5afd7c6ad557f5b
Ramya Prasad <Ramya.R.Prasad@ibm.com>
2020-05-01 17:06:15 -0400

    add touchable Opacity to images

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
       width: "100%",
       flexDirection: "row",
       justifyContent: "space-between",
-      paddingHorizontal: 20,
+      paddingHorizontal: 10,
       height: "100%",
       alignItems: "center",
       marginTop: 10,
@@ -70,12 +70,18 @@ class HomeScreen extends React.Component {
       this.showImageFunc = this.showImageFunc.bind(this);
       this.showEyeImageFunc = this.showEyeImageFunc.bind(this);
       this.showMouthImageFunc = this.showMouthImageFunc.bind(this);
-
+      this.selectTopImage = this.selectTopImage.bind(this);
     this.state = {
       editMode: false,
       showImage: false,
     showEyeImage: false,
-    showMouthImage: false
+    showMouthImage: false,
+    topImage: 0,
+    midImage:0,
+    bottomImage:0,
+    topImageFinal: 0,
+    midImageFinal:0,
+     bottomImageFinal:0,
     };
       
   }
@@ -113,7 +119,24 @@ class HomeScreen extends React.Component {
         this.setState({showImage: false});
 
     }
+    selectTopImage = (index) => {
+        this.setState({topImage: index});
 
+    }
+    selectMidImage = (index) => {
+           this.setState({midImage: index});
+
+       }
+    selectBottomImage = (index) => {
+           this.setState({bottomImage: index});
+
+       }
+    displayAvatar = () => {
+          this.setState({topImageFinal: topImage});
+        this.setState({midImageFinal: midImage});
+        this.setState({bottomImageFinal: bottomImage});
+
+    }
   render() {
     const { editMode } = this.state;
     const modalHeight = 2 * (Dimensions.get("screen").height / 3);
@@ -149,26 +172,29 @@ class HomeScreen extends React.Component {
           <TouchableOpacity onPress={()=>this.showMouthImageFunc()}>
             <Text style={{fontSize: 25, color: "#333333",textAlign: "left"}}>Mouth</Text>
           </TouchableOpacity>
-          <Button
-                         onPress={this.showMouthImageFunc}
-                         title="Hair"
-                         color="#841584"/>
         </View>
             
         <View style={styles.imageTabContainer}>
 
                 {this.state.showImage &&
                       <View>
+        <TouchableOpacity onPress={()=>this.selectTopImage("1")}>
                         <Image
                 style={{width: 100, height: 100, marginTop:20,marginRight: 5, marginLeft:30}}
                           source={require('../assets/avatar/top/brain.png')} />
-                      </View>}
+        </TouchableOpacity>
+
+        </View>}
             
             {this.state.showImage &&
               <View>
+                <TouchableOpacity onPress={()=>this.selectTopImage("2")}>
+
                 <Image
             style={{width: 100, height: 100, marginTop:20, marginRight: 30, marginLeft:5}}
                   source={require('../assets/avatar/top/hair.png')} />
+                </TouchableOpacity>
+
               </View>}
                 
         </View>
@@ -177,22 +203,33 @@ class HomeScreen extends React.Component {
 
                 {this.state.showEyeImage &&
                       <View>
+                    <TouchableOpacity onPress={()=>this.selectMidImage("1")}>
+
                         <Image
                 style={{width: 100, height: 100, marginTop:20,marginRight: 10, marginLeft:10}}
                           source={require('../assets/avatar/mid/cyclops.png')} />
+                    </TouchableOpacity>
+
                       </View>}
                        {this.state.showEyeImage &&
                              <View>
+                           <TouchableOpacity onPress={()=>this.selectMidImage("2")}>
+
                                <Image
                        style={{width: 100, height: 100, marginTop:20,marginRight: 10, marginLeft:10}}
                                  source={require('../assets/avatar/mid/goofy.png')} />
+                           </TouchableOpacity>
+
                              </View>}
                    
                    {this.state.showEyeImage &&
                      <View>
+                       <TouchableOpacity onPress={()=>this.selectMidImage("3")}>
                        <Image
                    style={{width: 150, height: 100, marginTop:20, marginRight: 10, marginLeft:10}}
                          source={require('../assets/avatar/mid/scared.png')} />
+                       </TouchableOpacity>
+
                      </View>}
                        
                </View>
@@ -200,20 +237,27 @@ class HomeScreen extends React.Component {
 
                  {this.state.showMouthImage &&
                                 <View>
+                     <TouchableOpacity onPress={()=>this.selectBottomImage("1")}>
+
                                   <Image
                           style={{width: 100, height: 100, marginTop:20,marginRight: 25, marginLeft:25}}
                                     source={require('../assets/avatar/bottom/toothy.png')} />
+                     </TouchableOpacity>
+
                                 </View>}
                       
                {this.state.showMouthImage &&
                         <View>
+                   <TouchableOpacity onPress={()=>this.selectBottomImage("2")}>
+
                           <Image
                       style={{width: 100, height: 100, marginTop:20, marginRight: 25, marginLeft:25}}
                             source={require('../assets/avatar/bottom/vamp.png')} />
-                        </View>}
-                          
+                   </TouchableOpacity>
+
+                        </View>}                          
         </View>
-          
+                        
             
         </BottomSheet>
       </View>
