6eccc273e5fad0d04ec7b6aec68cec479a6f2a8e
Yusra Syeda <yusra.syeda@ibm.com>
2020-05-03 16:38:19 -0400

    implement lessons that mimic online reading tutor lessons with: -10 questions -when option is pressed, new question appears

--- src/screens/LessonScreen.js
+++ src/screens/LessonScreen.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Constants from "../components/Constants.js";
-import { Text, View, Button, Alert } from "react-native";
+import { StyleSheet, Text, View, Button, Alert, ScrollView, TouchableOpacity } from "react-native";
+import { useState } from 'react';
 import {
   updateBadgeState,
   updateLessonBadgeState,
@@ -11,10 +12,65 @@ import {
   checkAndIssueStreaksBadge,
 } from "../components/Helpers";
 
+
+PlaySound=(item)=>{
+
+
+  Alert.alert(item);
+}
+
+
 function LessonScreen(props) {
+
+  q_and_a = [
+
+      { question: "dode", answer: ["dode", "dobe", "doke"]},
+      { question: "jafe", answer: ["jase", "jafe", "jale"]},
+      { question: "taje", answer: ["taje", "tade", "tabe"]},
+      { question: "loce", answer: ["loze", "lote", "loce"]},
+      { question: "bele", answer: ["bebe", "bele", "beke"]},
+      { question: "jafe", answer: ["jase", "jafe", "jale"]},
+      { question: "rike", answer: ["rike", "roke", "ruke"]},
+      { question: "gake", answer: ["gace", "guke", "gake"]},
+      { question: "pebe", answer: ["pabe", "pebe", "pibe"]},
+      { question: "mupe", answer: ["mipe", "mupe", "mape"]}
+
+    ];
+  const [index, setIndex] = useState(0);
+  const current = q_and_a[index];
+  console.log(current);
+
   return (
-    <View>
-      <Text>Lesson Simulation</Text>
+ 
+      <View style={styles.MainContainer}>
+       <Text style={styles.TextStyle} onPress={PlaySound.bind(this, current.question)} >{current.question}</Text>
+        <View style={{flex: 1, flexDirection: 'row', justifyContent: 'center'}}>
+          <Text style={styles.TextStyle} onPress={() => {
+                                          if(current.answer[0]===current.question){
+                                           setIndex(index + 1);
+                                          } else {
+                                            Alert.alert("Try again!");
+                                          }
+                                        }} 
+                                        > {current.answer[0]} </Text>
+          <Text style={styles.TextStyle} onPress={() => {
+                                          if(current.answer[1]===current.question){
+                                           setIndex(index + 1);
+                                          } else {
+                                            Alert.alert("Try again!");
+                                          }
+                                        }} 
+                                        > {current.answer[1]} </Text>
+          <Text style={styles.TextStyle} onPress={() => {
+                                          if(current.answer[2]===current.question){
+                                           setIndex(index + 1);
+                                          } else {
+                                            Alert.alert("Try again!");
+                                          }
+                                        }} 
+                                        > {current.answer[2]} </Text>
+        </View>
+      
       <Button
         title="Click here to finish lesson (go back to home screen)"
         onPress={() => {
@@ -32,4 +88,19 @@ function LessonScreen(props) {
   );
 }
 
+const styles = StyleSheet.create({
+
+ MainContainer: {
+   flex: 1,
+   margin: 10
+ },
+
+ TextStyle:{
+   fontSize : 30,
+    textAlign: 'center'
+ }
+ 
+});
+
 export default LessonScreen;
+
