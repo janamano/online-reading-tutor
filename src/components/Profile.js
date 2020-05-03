26dce410750d55b0dd88bc988347c8018e61a5d6
Ramya Prasad <31141615+rampras3@users.noreply.github.com>
2020-05-03 17:24:08 -0400

    update profile page to have constants (#61)

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -19,114 +19,113 @@ export default class Profile extends React.Component {
         this.showMouthImageFunc = this.showMouthImageFunc.bind(this);
         this.selectTopImage = this.selectTopImage.bind(this);
         this.state = {
-            editMode: false,
-            showImage: false,
-            showEyeImage: false,
-            showMouthImage: false,
-            topImage: 0,
-            midImage: 0,
-            bottomImage: 0,
-            topImageFinal: 0,
-            midImageFinal: 0,
-            bottomImageFinal: 0,
-            hair: null,
-            eyes: null,
-            mouth: null,
-            hairTextColour: "#333333",
-            eyeTextColour: "#333333",
-            mouthTextColour: "#333333",
-            Top: [
-                {
-                    "image": require("../assets/avatar/top/brain.png")
-                },
-                {
-                    "image": require("../assets/avatar/top/hair.png")
-                }
-            ],
-            Mid: [
-                {
-                    "image": require('../assets/avatar/mid/cyclops.png')
-                },
-                {
-                    "image": require('../assets/avatar/mid/goofy.png')
-                },
-                {
-                    "image": require('../assets/avatar/mid/scared.png')
-                }
-            ],
-            Bottom: [
-                {
-                    "image": require("../assets/avatar/bottom/toothy.png")
-                },
-                {
-                    "image": require("../assets/avatar/bottom/vamp.png")
-                }
-            ]
-
+          editMode: false,
+          showImage: false,
+          showEyeImage: false,
+          showMouthImage: false,
+          topImage: 0,
+          midImage:0,
+          bottomImage:0,
+          topImageFinal: 0,
+          midImageFinal:0,
+          bottomImageFinal:0,
+          hair : null,
+          eyes : null,
+          mouth : null,
+          hairTextColour : "#333333",
+          eyeTextColour: "#333333",
+          mouthTextColour: "#333333",
+          Top : [
+            {
+              "image": Constants.TOP_BRAIN
+            },
+            {
+              "image": Constants.TOP_HAIR
+            }
+         ],
+        Mid : [
+            {
+              "image": Constants.MID_CYCLOPS
+            },
+            {
+              "image": Constants.MID_GOOFY
+            },
+            {
+              "image": Constants.MID_SCARED
+            }
+         ],
+         Bottom : [
+            {
+               "image": Constants.BOT_TOOTHY
+            },
+            {
+               "image": Constants.BOT_VAMP
+            }
+         ]
+            
         };
-
-    }
-
-    toggleEditState() {
+          
+      }
+    
+      toggleEditState() {
         this.setState((prevState) => {
-            const newState = !prevState.editMode;
-            return {
-                editMode: newState,
-                showImage: false,
-                showEyeImage: false,
-                showMouthImage: false
-
-            };
+          const newState = !prevState.editMode;
+          return {
+            editMode: newState,
+            showImage:false,
+            showEyeImage: false,
+            showMouthImage: false
+              
+          };
         });
-    }
-
-    showImageFunc = () => {
-        this.setState({ hair: "underline", eye: null, mouth: null, hairTextColour: "white", eyeTextColour: "#333333", mouthTextColour: "#333333" });
-        this.setState({ showImage: true });
-        this.setState({ showEyeImage: false });
-        this.setState({ showMouthImage: false });
-    }
-    showEyeImageFunc = () => {
-        this.setState({ hair: null, eye: "underline", mouth: null, eyeTextColour: "white", hairTextColour: "#333333", mouthTextColour: "#333333" });
-        this.setState({ showEyeImage: true });
-        this.setState({ showImage: false });
-        this.setState({ showMouthImage: false });
-
-    }
-    showMouthImageFunc = () => {
-        this.setState({ hair: null, eye: null, mouth: "underline", mouthTextColour: "white", hairTextColour: "#333333", eyeTextColour: "#333333" });
-        this.setState({ showMouthImage: true });
-        this.setState({ showEyeImage: false });
-        this.setState({ showImage: false });
-    }
-    selectTopImage = (index) => {
-        this.setState({ topImage: index });
-    }
-    selectMidImage = (index) => {
-        this.setState({ midImage: index });
-    }
-    selectBottomImage = (index) => {
-        this.setState({ bottomImage: index });
-    }
-
-    displayAvatar = () => {
-        this.setState({ hair: null, eye: null, mouth: null, hairTextColour: "#333333", eyeTextColour: "#333333", mouthTextColour: "#333333" });
-        this.setState({ topImageFinal: this.state.topImage });
-        this.setState({ midImageFinal: this.state.midImage });
-        this.setState({ bottomImageFinal: this.state.bottomImage });
-        this.toggleEditState();
-    }
-    cancelSave = () => {
-        this.setState({ hair: null, eye: null, mouth: null, hairTextColour: "#333333", eyeTextColour: "#333333", mouthTextColour: "#333333" });
-        this.setState({ topImage: this.state.topImageFinal });
-        this.setState({ midImage: this.state.midImageFinal });
-        this.setState({ bottomImage: this.state.bottomImageFinal });
-        this.toggleEditState();
-    }
-
-    render() {
+      }
+        
+      showImageFunc = () => {
+          this.setState({hair:"underline",eye:null,mouth:null,hairTextColour:"white", eyeTextColour: "#333333", mouthTextColour:  "#333333"});
+          this.setState({showImage: true});
+          this.setState({showEyeImage: false});
+          this.setState({showMouthImage: false});
+      }
+      showEyeImageFunc = () => {
+          this.setState({hair:null,eye:"underline",mouth:null,eyeTextColour:"white", hairTextColour:  "#333333", mouthTextColour:  "#333333"});
+          this.setState({showEyeImage: true});
+          this.setState({showImage: false});
+          this.setState({showMouthImage: false});
+          
+      }
+      showMouthImageFunc = () => {
+          this.setState({hair:null,eye:null,mouth:"underline",mouthTextColour:"white",hairTextColour: "#333333",eyeTextColour:  "#333333"});
+          this.setState({showMouthImage: true});
+          this.setState({showEyeImage: false});
+          this.setState({showImage: false});
+      }
+      selectTopImage = (index) => {
+          this.setState({topImage: index});
+      }
+      selectMidImage = (index) => {
+          this.setState({midImage: index});
+      }
+      selectBottomImage = (index) => {
+          this.setState({bottomImage: index});
+      }
+        
+      displayAvatar = () => {
+          this.setState({hair:null,eye:null,mouth:null,hairTextColour: "#333333", eyeTextColour: "#333333", mouthTextColour:  "#333333"});
+          this.setState({topImageFinal: this.state.topImage});
+          this.setState({midImageFinal: this.state.midImage});
+          this.setState({bottomImageFinal: this.state.bottomImage});
+          this.toggleEditState();
+      }
+      cancelSave = () => {
+          this.setState({hair:null,eye:null,mouth:null,hairTextColour: "#333333", eyeTextColour: "#333333", mouthTextColour:  "#333333"});
+          this.setState({topImage: this.state.topImageFinal});
+          this.setState({midImage: this.state.midImageFinal});
+          this.setState({bottomImage: this.state.bottomImageFinal});
+          this.toggleEditState();
+      }
+      render() {
         const { editMode } = this.state;
-        const modalHeight = 2 * (Dimensions.get("screen").height / 3);
+        const modalHeight = 2 * (Dimensions.get("screen").height * 0.27);
         return (
             <SafeAreaView style={styles.container}>
                 <View style={styles.topContainer}>
@@ -179,78 +178,78 @@ export default class Profile extends React.Component {
                 </View>
 
                 <BottomSheet onDismiss={() => this.toggleEditState()} visible={editMode} height={modalHeight}>
-                    <View style={styles.tabContainer}>
-                        <TouchableOpacity onPress={() => this.showImageFunc()}>
-                            <Text style={{ fontSize: 25, color: this.state.hairTextColour, textDecorationLine: this.state.hair }}>Hair</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => this.showEyeImageFunc()}>
-                            <Text style={{ fontSize: 25, color: this.state.eyeTextColour, textDecorationLine: this.state.eye }}>Eyes</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => this.showMouthImageFunc()}>
-                            <Text style={{ fontSize: 25, color: this.state.mouthTextColour, textDecorationLine: this.state.mouth }}>Mouth</Text>
-                        </TouchableOpacity>
-                    </View>
+          <View style={styles.tabContainer}>
+            <TouchableOpacity onPress={()=>this.showImageFunc()}>
+              <Text style={{ fontSize: 25, color:this.state.hairTextColour, textDecorationLine: this.state.hair }}>Hair</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>this.showEyeImageFunc()}>
+              <Text style={{ fontSize: 25, color:this.state.eyeTextColour, textDecorationLine: this.state.eye }}>Eyes</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={()=>this.showMouthImageFunc()}>
+              <Text style={{ fontSize: 25, color:this.state.mouthTextColour, textDecorationLine: this.state.mouth }}>Mouth</Text>
+            </TouchableOpacity>
+          </View>
+            
+          <View style={styles.imageTabContainer}>
+            {this.state.showImage &&
+              <View>
+                <TouchableOpacity onPress={()=>this.selectTopImage(0)}>
+                  <Image style={styles.topBodyPart}
+                  source={Constants.TOP_BRAIN} />
+                </TouchableOpacity>
+              </View>}
+            
+            {this.state.showImage &&
+              <View>
+                <TouchableOpacity onPress={()=>this.selectTopImage(1)}>
+                  <Image style={styles.topBodyPart}
+                    source={Constants.TOP_HAIR} />
+                </TouchableOpacity>
+              </View>}
+                
+            {this.state.showEyeImage &&
+              <View>
+                <TouchableOpacity onPress={()=>this.selectMidImage(0)}>
+                  <Image
+                source={Constants.MID_CYCLOPS} />
+                </TouchableOpacity>
+              </View>}
+            
+            {this.state.showEyeImage &&
+              <View>
+                <TouchableOpacity onPress={()=>this.selectMidImage(1)}>
+                  <Image
+                source={Constants.MID_GOOFY} />
+                </TouchableOpacity>
+              </View>}
+                   
+            {this.state.showEyeImage &&
+              <View>
+                <TouchableOpacity onPress={()=>this.selectMidImage(2)}>
+                  <Image
+                source={Constants.MID_SCARED} />
+                </TouchableOpacity>
+              </View>}
+  
+            {this.state.showMouthImage &&
+              <View>
+                <TouchableOpacity onPress={()=>this.selectBottomImage(0)}>
+                  <Image style={styles.bottomBodyPart}
+                source={Constants.BOT_TOOTHY} />
+                </TouchableOpacity>
 
-                    <View style={styles.imageTabContainer}>
-                        {this.state.showImage &&
-                            <View>
-                                <TouchableOpacity onPress={() => this.selectTopImage(0)}>
-                                    <Image style={styles.topBodyPart}
-                                        source={require('../assets/avatar/top/brain.png')} />
-                                </TouchableOpacity>
-                            </View>}
-
-                        {this.state.showImage &&
-                            <View>
-                                <TouchableOpacity onPress={() => this.selectTopImage(1)}>
-                                    <Image style={styles.topBodyPart}
-                                        source={require('../assets/avatar/top/hair.png')} />
-                                </TouchableOpacity>
-                            </View>}
-
-                        {this.state.showEyeImage &&
-                            <View>
-                                <TouchableOpacity onPress={() => this.selectMidImage(0)}>
-                                    <Image
-                                        source={require('../assets/avatar/mid/cyclops.png')} />
-                                </TouchableOpacity>
-                            </View>}
-
-                        {this.state.showEyeImage &&
-                            <View>
-                                <TouchableOpacity onPress={() => this.selectMidImage(1)}>
-                                    <Image
-                                        source={require('../assets/avatar/mid/goofy.png')} />
-                                </TouchableOpacity>
-                            </View>}
-
-                        {this.state.showEyeImage &&
-                            <View>
-                                <TouchableOpacity onPress={() => this.selectMidImage(2)}>
-                                    <Image
-                                        source={require('../assets/avatar/mid/scared.png')} />
-                                </TouchableOpacity>
-                            </View>}
-
-                        {this.state.showMouthImage &&
-                            <View>
-                                <TouchableOpacity onPress={() => this.selectBottomImage(0)}>
-                                    <Image style={styles.bottomBodyPart}
-                                        source={require('../assets/avatar/bottom/toothy.png')} />
-                                </TouchableOpacity>
-
-                            </View>}
-
-                        {this.state.showMouthImage &&
-                            <View>
-                                <TouchableOpacity onPress={() => this.selectBottomImage(1)}>
-                                    <Image style={styles.bottomBodyPart}
-                                        source={require('../assets/avatar/bottom/vamp.png')} />
-                                </TouchableOpacity>
-                            </View>}
-
-                    </View>
-                    <View>
+              </View>}
+                      
+            {this.state.showMouthImage &&
+              <View>
+                <TouchableOpacity onPress={()=>this.selectBottomImage(1)}>
+                  <Image style={styles.bottomBodyPart}
+                source={Constants.BOT_VAMP} />
+                </TouchableOpacity>
+              </View>}
+                   
+          </View>
+          <View>
                     <View>
                             <TouchableOpacity style={globalStyles.primaryBtn} onPress={() => this.displayAvatar()} >
                             <Text style={globalStyles.primaryBtnText}>Save</Text>
